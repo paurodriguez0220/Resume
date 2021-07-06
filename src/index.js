316e46ef8f58db98a1b2caaac31bbb302398622c
Paulo CuriousDev <52302397+paurodriguez0220@users.noreply.github.com>
2021-07-06 21:09:18 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Resume from './component/Resume/Resume';
 import { BrowserRouter as Router, Route  } from 'react-router-dom';
 
 ReactDOM.render(
-  <Router basename={'/Resume/'}>
+  <Router basename={'/resume'}>
     <Route path="/" component={Resume} />
   </Router>,
   document.getElementById('root')
